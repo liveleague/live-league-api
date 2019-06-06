99e3f7f9ab850e7076a4ec099e5f34671b162e39
Jack Sellers <jackwsellers@gmail.com>
2019-06-06 14:47:38 +0100

    Created user admin page.

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@test.com',
+            password='adminpass'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@test.com',
+            password='testpass',
+            name='Test User',
+        )
+
+    def test_users_listed(self):
+        """Test that users are listed on the admin's user page."""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+        self.assertContains(res, self.user.email)
+        self.assertContains(res, self.user.name)
+
+    def test_edit_user_page(self):
+        """Test that the admin's user edit page works."""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that the admin's create user page works."""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
